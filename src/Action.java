9919cb6a53a3b8db26fc7f2a63336d5485642225
isuru0 <isurushehara@gmail.com>
2025-07-06 19:48:05 +0530

    Upload the project :

---
+++ src/Action.java
@@ -0,0 +1,136 @@
+public class Action {
+
+    //CHECK WIN OR NOT
+    boolean win = false;
+    //COUNT THE COMMANDS
+    int command_count = 0;
+
+    //MAP DETAILS
+    int[][] map_1 = {
+            {1,1,1,1,0,1,1,1,1,1},
+            {1,0,1,0,0,0,1,0,0,1},
+            {1,0,1,0,1,0,1,1,0,1},
+            {1,0,0,0,1,1,1,0,0,1},
+            {1,1,1,0,0,0,0,0,1,1},
+            {1,0,0,1,1,1,1,0,1,1},
+            {1,0,0,1,0,0,0,0,1,1},
+            {1,0,1,1,0,1,1,0,1,1},
+            {1,0,0,0,0,0,0,0,0,1},
+            {1,1,1,1,0,1,1,1,1,1}};
+    int[][] map = map_1;
+    int row = 0, column = 4; //DEFAULT
+    int position_value = map[row][column];
+
+    //TURN ACTIONS
+    //VARIABLES
+    String[] array_direction = {"north","east","south","west"};
+    int dir_turn = 0; //DEFAULT
+    String direction = array_direction[dir_turn];
+
+    //TURN ACTION METHOD (TRUE MEAN TURN RIGHT)
+    void turn(boolean turn){
+        if(turn && dir_turn != 3)
+            dir_turn++;
+        else if(turn && dir_turn == 3)
+            dir_turn = 0;
+        else if(!turn && dir_turn != 0)
+            dir_turn--;
+        else if(!turn && dir_turn == 0)
+            dir_turn = 3;
+
+        direction = array_direction[dir_turn];
+        command_count++;
+    }
+
+    //FORWARD ACTION METHOD
+    void go(){
+        switch (direction){
+            case "north":
+                row++;
+                if(map[row][column] == 1)
+                {
+                    System.out.println("Can't go, there is a wall");
+                    row--;
+                }
+                else if(map[row][column] == 0 && row==9 && column==4)
+                {
+                    System.out.println("Congratulations! You are free.");
+                    System.out.println("You use " +command_count +" commands.");
+                    win = true;
+
+                }
+                else if(map[row][column] == 0)
+                {
+                    System.out.println("Way is clear and go");
+                }
+                break;
+
+            case "east":
+                column--;
+                if(map[row][column] == 1)
+                {
+                    System.out.println("Can't go, there is a wall");
+                    column++;
+                }
+                else if(map[row][column] == 0 && row==9 && column==4)
+                {
+                    System.out.println("Congratulations! You are free.");
+                    System.out.println("You use " +command_count +" commands.");
+                    win = true;
+                }
+                else if(map[row][column] == 0)
+                {
+                    System.out.println("Way is clear and go");
+                }
+                break;
+
+            case "south":
+                row--;
+                if(map[row][column] == 1)
+                {
+                    System.out.println("Can't go, there is a wall");
+                    row++;
+                }
+                else if(map[row][column] == 0 && row==9 && column==4)
+                {
+                    System.out.println("Congratulations! You are free.");
+                    System.out.println("You use " +command_count +" commands.");
+                    win = true;
+                }
+                else if(map[row][column] == 0)
+                {
+                    System.out.println("Way is clear and go");
+                }
+                break;
+
+            case "west":
+                column++;
+                if(map[row][column] == 1)
+                {
+                    System.out.println("Can't go, there is a wall");
+                    column--;
+                }
+                else if(map[row][column] == 0 && row==9 && column==4)
+                {
+                    System.out.println("Congratulations! You are free.");
+                    System.out.println("You use " +command_count +" commands.");
+                    win = true;
+                }
+                else if(map[row][column] == 0)
+                {
+                    System.out.println("Way is clear and go");
+                }
+                break;
+        }
+        command_count++;
+    }
+
+    //LOCATION METHOD
+    void where() {
+        int relative_column = 9 - column;
+        int relative_row = row + 1;
+        System.out.println("Direction:" +direction +"\nx:" +relative_column +" y:" +relative_row);
+        command_count++;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main extends Action{
+
+    public static void main(String[] args) {
+
+        //CREATE INPUT OBJECT AND PLAYER OBJECT
+        Scanner input = new Scanner(System.in);
+        Action player = new Action();
+
+        //INTRODUCTIONS
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("||------------------------------------------------------------||");
+        System.out.println("|| Your are enter the Maze that build using 10x10 blocks.     ||");
+        System.out.println("|| x=1 and x=10 are the side walls                            ||");
+        System.out.println("|| Start position is x:5 y:1.                                 ||");
+        System.out.println("|| These are commands: go, turn right, turn left, where, exit ||");
+        System.out.println("||------------------------------------------------------------||");
+        System.out.println("----------------------------------------------------------------");
+
+        //PLAY TIMES
+        for(int i = 0; i<100; i++)
+        {
+            //GET USER INPUT UNTIL WIN THE GAME
+            if(!player.win)
+            {
+                //GET USER INPUT
+                System.out.print("-> Enter you command: ");
+                String get_command = input.nextLine();
+
+                //CALL METHODS ACCORDING TO USER'S INPUTS
+                switch (get_command) {
+                    case "go" -> player.go();
+                    case "turn right" -> player.turn(true);
+                    case "turn left" -> player.turn(false);
+                    case "where" -> player.where();
+                    case "exit" -> i = 100;
+                    default ->
+                            System.out.println("Invalid command!\nThese are commands: go, turn right, turn left, where, exit");
+                }
+            }
+            else if (player.win)
+                i = 100;
+        }
+    }
+}
